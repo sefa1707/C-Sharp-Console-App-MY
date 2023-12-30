df602b000a079fd82b4d9193f0607794923090a5
Sefa AŞIK <sefa.a1707@gmail.com>
2023-12-30 23:43:05 +0300

    Add project files.

---
+++ C-Sharp-Console-App-MY/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Console_App_MY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Video Başlangıcı
+
+            //İlk komutumuz Merha Dünya
+            //Console.Write("Merhaba Dünya"); //Bu komutu tek başına kullanırsak siyah ekranda merhaba dünya yazısı gelir ve hemen kapanır.
+
+            //Console.Write("Merhaba Dünya"); 
+            //Console.Read(); //Klavyeden bir tuşa basılmasını bekler.Bizim burada bu kodu kullanma amacımız merhaba dünya yazısı biz bir tuşa basana kadar kalması için.
+
+            //Console.WriteLine("Merhaba Dünya");//Yazdığımız ifadenin tüm satırı kaplamasını ve bir sonraki işlemin bir alt satırdan başlamasını sağlar.
+            //Console.Read();
+        }
+    }
+}
